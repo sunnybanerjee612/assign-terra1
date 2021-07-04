1706b99b620d1ee9cd66090a5902f1a59d29f843
sunnybanerjee612 <83915611+sunnybanerjee612@users.noreply.github.com>
2021-07-03 23:19:57 -0500

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -2,7 +2,7 @@ resource "aws_ami_copy" "example" {
   name              = "terraform-example"
   description       = "A copy of ami-xxxxxxxx"
   source_ami_id     = "ami-xxxxxxxx"
-  source_ami_region = "us-west-1"
+  source_ami_region = "us-east-1"
 
   tags = {
     Name = "HelloWorld"
