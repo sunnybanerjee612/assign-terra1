232d08881416ec8720ed6da4ea5b028e837c67dc
sunnybanerjee612 <83915611+sunnybanerjee612@users.noreply.github.com>
2021-07-03 22:55:44 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,8 +2,7 @@ resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket"
   acl    = "private"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+  versioning {
+    enabled = true
   }
 }
